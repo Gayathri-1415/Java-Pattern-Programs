505ac0d00be941fc848e4ac18abd2027e2476de8
Gayathri-1415 <kothapalligayathri147@gmail.com>
2026-02-17 20:47:59 +0530

    pattern1 posted

---
+++ Pattern01_RightTriangleSameNumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Pattern01_RightTriangleSameNumber
+ {
+    public static void main(String[] args) {
+       
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no of rows: ");
+        int n=sc.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
