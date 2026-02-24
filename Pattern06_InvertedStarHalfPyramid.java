168686dc74b590f49fae923ad375077a5816e606
Gayathri-1415 <kothapalligayathri147@gmail.com>
2026-02-24 22:17:10 +0530

    pattern6 posted

---
+++ Pattern06_InvertedStarHalfPyramid.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Pattern06_InvertedStarHalfPyramid {
+    public static void main(String[] args)
+     {
+        Scanner sc=new Scanner(System.in);
+        int i,j;
+        System.out.println("Enter no of rows: ");
+        int n=sc.nextInt();
+        for(i=n;i>=1;i--) 
+            {
+                for(j=1;j<=i;j++)
+                {
+                    System.out.print("*"+" ");
+                }
+                System.out.println();
+            }       
+    }
+}
