3ba71ebfe95f61aacb7d13e85d9bf3b10cb15142
Gayathri-1415 <kothapalligayathri147@gmail.com>
2026-03-02 13:27:19 +0530

    pattern9 posted

---
+++ Pattern09_DiamondStar.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Pattern09_DiamondStar
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,j;
+        System.out.println("Enter no of rows");
+        int n=sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=1;j<=2*i-1;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(i=n-1;i>=1;i--)
+        {
+            for(j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=1;j<=2*i-1;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
